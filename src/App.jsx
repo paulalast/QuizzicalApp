28385ff02d856f02c3ccb0779424e9c2077d55a6
Paulina <paulalastowiecka@gmail.com>
2023-05-23 12:11:09 +0200

    quiz screen vizualisation, style, random question, and answers, still without possibility to check the correct answs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import QuestionBox from "./QuestionBox"
-
-// https://opentdb.com/api.php?amount=5   API
 
 function WelcomeScreen({ onStart }) {
 	return (
-		<body>
+		<div>
 			<main className='welcome-screen'>
 				<h1>Quizzical</h1>
 				<h2>
@@ -14,38 +12,59 @@ function WelcomeScreen({ onStart }) {
 				</h2>
 				<button onClick={onStart}>Start quiz</button>
 			</main>
-		</body>
+		</div>
 	)
 }
-function getQA() {
-	fetch(
-		"https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple"
-	)
-		.then(res => res.json())
-		.then(data => {
-			console.log(data.results[0].question) 
-			console.log(data.results[0].correct_answer)
-			console.log(data.results[0].incorrect_answers)
-		
+async function getQA() {
+	try {
+		const res = await fetch(
+			"https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple"
+		)
+		const data = await res.json()
+
+		return data.results.map(result => {
+			const answers = [result.correct_answer, ...result.incorrect_answers]
+			for (let i = answers.length - 1; i > 0; i--) {
+				const j = Math.floor(Math.random() * (i + 1))
+				;[answers[i], answers[j]] = [answers[j], answers[i]]
+			}
+			return {
+				question: result.question,
+				answers: answers,
+				correctAnswer: result.correct_answer,
+			}
 		})
-		.catch(error => {
-			console.log("error:", error)
-		})
+	} catch (error) {
+		console.log("error:", error)
+		return null
+	}
 }
-getQA()
 
 function QuizScreen() {
+	const [questions, setQuestions] = useState([])
+
+	useEffect(() => {
+		getQA().then(data => {
+			if (data) {
+				setQuestions(data)
+			}
+		})
+	}, [])
 	return (
-		<body>
+		<div>
 			<main className='quiz-screen'>
-				<QuestionBox />
-				<QuestionBox />
-				<QuestionBox />
-				<QuestionBox />
-				<QuestionBox />
+				{questions.map((questionData, index) => (
+					<QuestionBox
+						key={index}
+						question={questionData.question}
+						answers={questionData.answers}
+						questionIndex={index}
+					/>
+				))}
+
 				<button>Check answers</button>
 			</main>
-		</body>
+		</div>
 	)
 }
 
@@ -64,3 +83,11 @@ function App() {
 }
 
 export default App
+
+/* 
+1. Add posibility to check the answers
+2. Count the correct answers
+3. If 5/5 answers confetti 
+4. Third screen - showing the correct and wrong answers - if answ checked is correct green, is wrong change color on red, and color the correct answ
+5. show how many answ is correct 2/5
+ */
--- src/QuestionBox.jsx
+++ src/QuestionBox.jsx
@@ -1,14 +1,26 @@
 import React from "react"
 
-function QuestionBox() {
+function QuestionBox({ question, answers, questionIndex }) {
+    function normalText(text) {
+        const textArea = document.createElement("textarea");
+        textArea.innerHTML = text;
+        return textArea.value;
+      }
 	return (
 		<div className='question-box'>
-			<h2>Question?</h2>
+			<h2>{normalText(question)}</h2>
 			<div className='answers-box'>
-				<button type='radio'> Answer answer lalal lalala </button>
-				<button type='radio'> Answer</button>
-				<button type='radio'> Answer</button>
-				<button type='radio'> Answer</button>
+				{answers.map((answer, index) => (
+					<div key={index}>
+						<input
+							type='radio'
+							id={`answer-${questionIndex}-${index}`}
+							name={`question-${questionIndex}`}
+							value={answer}
+						/>
+						<label htmlFor={`answer-${questionIndex}-${index}`}>{normalText(answer)}</label>
+					</div>
+				))}
 			</div>
 		</div>
 	)
