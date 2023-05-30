9e4e7b5d4565a8491b85312dc0a578e0a80d3c54
Paulina <paulalastowiecka@gmail.com>
2023-05-30 14:32:31 +0200

    new2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/paulalast/QuizzicalApp",
-  plugins: [react()],
+	base: "https://github.com/paulalast/QuizzicalApp.git",
+	plugins: [react()],
 })
