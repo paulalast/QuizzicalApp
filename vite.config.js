3db091c97531069cfc5d73313dea4b8f28bea3ce
Paulina <paulalastowiecka@gmail.com>
2023-05-30 15:24:53 +0200

    n2

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,25 @@
-// import { defineConfig } from "vite"
-// import react from "@vitejs/plugin-react"
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-// 	base: "https://github.com/paulalast/QuizzicalApp.git",
-// 	plugins: [react()],
-// })
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/https://github.com/paulalast/QuizzicalApp.git',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/https://github.com/paulalast/QuizzicalApp.git/'
-  }
-
-  return config
+export default defineConfig({
+	base: "https://github.com/paulalast/QuizzicalApp.git",
+	plugins: [react()],
 })
+// vite.config.js
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ command }) => {
+//   const config = {
+//     plugins: [react()],
+//     base: '/https://github.com/paulalast/QuizzicalApp.git',
+//   }
+
+//   if (command !== 'serve') {
+//     config.base = '/https://github.com/paulalast/QuizzicalApp.git/'
+//   }
+
+//   return config
+// })
